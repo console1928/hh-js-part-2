f82266ee453eadf4a311cca992e9754502a7570a
Admin <riverflow1928@yandex.ru>
2020-12-29 15:25:53 +0500

    Add dynamic port

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 require("dotenv").config({ path: ".env" });
 
 const apiKey = process.env.API_KEY;
+const port = process.env.PORT;
 
 const config = {
-    apiKey
+    apiKey,
+    port
 };
 
 module.exports = config;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+const config = require("./config");
 const express = require("express");
 const app = express();
 
 app.use("/", express.static("./src"));
 
-app.listen(3000);
+app.listen(config.port);
